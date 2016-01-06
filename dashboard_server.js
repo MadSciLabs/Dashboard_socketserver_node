9ac0f4cc0522c52256a1d9202bd9ac0acf63aa0a
root <root@ip-10-250-19-33.us-west-2.compute.internal>
2016-01-06 20:38:44 +0000

    Update dashboard

--- dashboard_server.js
+++ dashboard_server.js
@@ -6,18 +6,32 @@ var request = require('request');
 var express = require('express');
 var server;
 
+var arrSymbols = ['GOOG', 'AAPL', 'NDAQ', 'FB' ];
+var _currIndex = 0;
+
 var app = express();
 app.use(express.static('public'));
 app.use(express.static('js'));
 
 console.log("START");
 
+var _stockArr = "";
+
+//MAKE SYMBOL STRING
+for (var i=0; i<arrSymbols.length; i++) {
+	if (i>0) {_stockArr += ",";}
+	_stockArr += arrSymbols[i];
+}
+
+var sPhoton1 = "280045000447343233323032";
+var sPhoton2 = "20001f000d47343432313031";
+
+var sSlackAction = "led";
+var sStockAction = "stockPrice";
+
 app.get('/slack', function(req,res) {
 
 	//TD Prototype #2
-	var _url_tdprototype2 = "https://api.particle.io/v1/devices/20001f000d47343432313031/led?access_token=e373cabcfaf640695f68f947d2070635c359cf0c";
-	var _url_tdprototype1 = "https://api.particle.io/v1/devices/280045000447343233323032/led?access_token=e373cabcfaf640695f68f947d2070635c359cf0c";
-
 	console.log("slack");
 	console.log(req.query);
 	console.log(req.query.text);
@@ -32,34 +46,40 @@ app.get('/slack', function(req,res) {
 	}
 
 	//Send out posts
-	_url = "";
+	_sPhoton = "";
+
 	switch (_cmd) {
 		case "td1":
 
-			_url = _url_tdprototype1;
+			_sPhoton = sPhoton1; 
 			break;
 		case "td2":
 
-			_url = _url_tdprototype2;
+			_sPhoton = sPhoton2;
 			break;
 
 		case "cat":
-			_url = "";
+			_sPhoton = "";
 			break;
 	}
+
+	var _url = "https://api.particle.io/v1/devices/" + _sPhoton + "/" + sSlackAction + "?access_token=e373cabcfaf640695f68f947d2070635c359cf0c";
+
+	console.log("SLACK URL>" + _url);
 
 	request.post(
 		_url,
 		{ form: { args: _val }},
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				console.log(body);
+				console.log("RESPONSE SLACK:" + body);
 			}
+			console.log("RESPONSE SLACK:" + response.statusCode);
 		}
 	);
 
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({a:1}));
+        res.send(JSON.stringify({status: "ok"}));
 });
 
 app.get('/dashboard.html', function(req,res) {
@@ -70,77 +90,92 @@ app.listen(80,function () {
         console.log("started on port 80");
 });
 
+//Get Stock Prices
 /*
-server = http.createServer(function(req, res){
+function get_stock()
+{
+    var url = "http://query.yahooapis.com/v1/public/yql";
+    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + arrSymbols[_currIndex] + "')");
 
-    // your normal server code
-    var path = url.parse(req.url).pathname;
-    switch (path){
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
-            res.end();
-            break;
-        //case '/test.html':
-        case '/dashboard.html':
-        //case '/stl.html':
-            fs.readFile(__dirname + path, function(err, data){
-                if (err){ 
-                    return send404(res);
-                }
-                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(data, 'utf8');
-                res.end();
-            });
-        break;
-        default: send404(res);
-    }
-}),
+    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+        .done(function (data) {
+    		
+    	console.log(">" + data.query.results.quote["LastTradePriceOnly"] + ">" + data.query.results.quote["Open"]);
 
-send404 = function(res){
-    res.writeHead(404);
-    res.write('404');
-    res.end();
-};
-
-server.listen(80);
-*/
-
-/*
-// use socket.io
-var io = require('socket.io').listen(server);
-
-var _client_light = 0;
-var dirty = true;
-
-//turn off debug
-io.set('log level', 1);
-
-// define interactions with client
-io.sockets.on('connection', function(socket){
-
-    //send data to client
-    setInterval(function(){
-        socket.emit('client_light', {'val': Math.floor((Math.random() * 200) + 1)});
-    }, 100);
-
-    //recieve client data
-    socket.on('rpi_light', function(data){
-
-	dirty = true;
-        //process.stdout.write(data.val + "\n");
-	console.log("in " + data.val);
-        _client_light = data.val; 
-
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+            $("#result").text('Request failed: ' + err);
     });
 
-    //SEND OUT
-    setInterval(function(){
-	if (true) { dirty = false;
-        socket.emit('client_light', {'val': _client_light});
-        }
-    }, 10);
+    setTimeout(get_stock, 1000);
 
-});
-
+}
 */
+
+/*
+	request.post(
+		_url,
+		{ form: { args: _val }},
+		function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+			}
+		}
+	);
+*/
+
+function get_stock()
+{
+	_sPhoton = sPhoton1;
+	_stock_url = "http://finance.google.com/finance/info?client=ig&q=NASDAQ:" + _stockArr;
+
+	//GET ALL STOCK FROM GOOGLE
+	request(_stock_url, function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+
+			//Take off leading symbol
+			_json = JSON.parse(body.substring(4, body.length - 1));
+
+			var _stock_out = "";
+
+			for (var i=0; i<4; i++)
+			{
+				//console.log(_json[i]["pcls_fix"]);
+				//console.log(_json[i]["l_cur"]);
+
+				//create string
+				_price = _json[i]["l_cur"] - _json[i]["pcls_fix"];
+				console.log(">" + _json[i]["l_cur"] + ">" +  _json[i]["pcls_fix"]);
+
+				_stock_out = _price.toFixed(2) + "," + _stock_out
+			}
+
+			console.log(_stock_out);
+
+			_url = "https://api.particle.io/v1/devices/" + _sPhoton + "/" + sStockAction + "?access_token=e373cabcfaf640695f68f947d2070635c359cf0c";
+
+			console.log(">" + _url);
+
+			request.post(
+				_url,
+				{ form: { args: _stock_out }},
+				function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						console.log("RESPONSE STOCK:" + body);
+					}
+					console.log("RESPONSE STOCK:" + response.statusCode);
+		
+					//get it again
+					setTimeout(get_stock, 5000);
+				}
+			);
+		}
+	});
+
+}
+
+//Get stocks
+get_stock();
+
